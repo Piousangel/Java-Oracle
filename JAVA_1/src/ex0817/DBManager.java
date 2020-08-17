d0f5def162ef4ef0d044c8d0c1a6d8ff19a9dca8
Piousangel <yyh7654@gmail.com>
2020-08-17 12:19:32 +0900

    why not?

--- JAVA_1/src/ex0817/DBManager.java
+++ JAVA_1/src/ex0817/DBManager.java
@@ -1,9 +1,25 @@
 package ex0817;
 
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+import java.net.*;
+
 public class DBManager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		finally {
+			
+			
+		}
 
 	}
 
